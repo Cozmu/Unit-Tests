bc48c6ccefe47594ca04979e40a1cabd67b11f6d
Cozmu <www.jorgereis@gmail.com>
2022-08-18 01:57:23 -0300

    Adicionando requisito 6

--- src/productDetails.js
+++ src/productDetails.js
@@ -41,5 +41,5 @@ const productDetails = (firstProduct, secondProduct) => [
     },
   },
 ];
-
+ console.log(productDetails('legal')[1].details.productId);
 module.exports = productDetails;
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,14 +30,25 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+  it('Teste se productDetails é uma função.', () => {
+    expect(typeof productDetails).toBe('function');
+  });
+  it('Teste se o retorno da função é um array.', () => {
+    expect(Array.isArray(productDetails())).toBe(true);
+  });
+  it('Teste se o array retornado pela função contém dois itens dentro.', () => {
+    expect(productDetails()).toHaveLength(2);
+  });
+  it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
+    expect(typeof productDetails()[0]).toBe('object');
+    expect(typeof productDetails()[0]).toBe('object');
+  });
+  it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
+    expect(productDetails('um','dois')[0]).not.toEqual(productDetails('um','dois')[1]);
+    // expect(productDetails('algodão', 'pirulito')[0]).not.toMatchObject(productDetails('algodão','pirulito')[1]) 
+  });
+  it('Teste se os dois productIds terminam com 123.', () => {
+    expect(productDetails('um', 'dois')[0].details.productId).toBe('um123');
+    expect(productDetails('um', 'dois')[1].details.productId).toBe('dois123');
   });
 });
